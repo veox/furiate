affd4e65daf805067a7ce1b9d5c97de4c8d21fe8
Noel Maersk <veox+gits@veox.pw>
2018-03-23 18:59:30 +0200

    furiate: as-is.

---
+++ furiate.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import getpass
+
+from web3 import Web3, HTTPProvider
+
+with open('infura.key') as keyfile:
+    infurakey = keyfile.read()
+
+chainids = {
+    #'mainnet': 1,
+    'ropsten': 3,
+    'rinkeby': 4,
+    'kovan': 42
+}
+w3s = {net: Web3(HTTPProvider('https://' + net + '.infura.io/' + infurakey)) for net in chainids.keys()}
+
+# https://web3py.readthedocs.io/en/latest/middleware.html#geth-style-proof-of-authority
+if 'rinkeby' in w3s.keys():
+    from web3.middleware import geth_poa_middleware
+    w3s['rinkeby'].middleware_stack.inject(geth_poa_middleware, layer=0)
+
+acct = None
+for net, w3 in w3s.items():
+    # enable local key management (disabled by default in v4 beta 12)
+    #w3.eth.enable_unaudited_features()
+
+    # only ask for password once
+    if acct is None:
+        with open('ethereum.key') as keyfile:
+            privkey = w3.eth.account.decrypt(keyfile.read(), getpass.getpass())
+            acct = w3.eth.account.privateKeyToAccount(privkey)
+
+    latest = w3.eth.getBlock('latest')['number']
+    print(net, latest)
+
+    tx = {
+        # specify explicitly to prevent accidental repeats
+        'nonce': 0,
+        # TODO: actual data
+        'to': acct.address,
+        # TODO: use estimateGas
+        'gas': 90000,
+        # TODO: don't rely on infura
+        'gasPrice': w3.eth.gasPrice,
+        # disable replay protection
+        'chainId': chainids[net]
+    }
+
+    # NOTE: could tx['chainId']=0 to disable replay protection and use same raw tx everywhere
+    signed = w3.eth.account.signTransaction(tx, privkey)
+
+    txhash = None
+    try:
+        txhash = w3.eth.sendRawTransaction(signed.rawTransaction)
+    except ValueError as e:
+        if e.args[0]['message'] != 'nonce too low':
+            raise e
+        else:
+            print(net, 'Transaction with nonce', tx['nonce'], 'already exists!..')
+    finally:
+        print(net, Web3.toHex(txhash))
