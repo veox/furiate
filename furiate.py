98c7f0ac1408dc12bdeb423005f398f9926a3e17
Noel Maersk <veox+gits@veox.pw>
2018-03-28 16:38:48 +0300

    furiate,schedule: rework schedule to be a multi-tx dict.

--- furiate.py
+++ furiate.py
@@ -6,11 +6,7 @@ import time
 from web3 import Web3, HTTPProvider
 from eth_account import Account # will use directly instead of through web3 provider
 
-from schedule import chainids, tx
-
-# flop if gas too low
-if tx['to'] == '' and tx['data'] != '' and tx['gas'] <= 100000:
-    raise Exception('Gas quite likely too low for deployment!')
+from schedule import chainids, txs
 
 with open('infura.key') as keyfile:
     infurakey = keyfile.read()
@@ -22,41 +18,38 @@ if 'rinkeby' in w3s.keys():
     from web3.middleware import geth_poa_middleware
     w3s['rinkeby'].middleware_stack.inject(geth_poa_middleware, layer=0)
 
-# check that nonces are the same on all chains
-nonces = {}
-for net, w3 in w3s.items():
-    print(net, 'block', w3.eth.getBlock('latest')['number'])
-    nonces[net] = w3.eth.getTransactionCount(acct.address)
-if len(set(list(nonces.values()))) > 1:
-    print('OOPS! nonces:', nonces)
-    raise Exception('Nonces do not line up!')
-
 # only ask for password once
 with open('ethereum.key') as keyfile:
     privkey = Account.decrypt(keyfile.read(), getpass.getpass())
     acct = Account.privateKeyToAccount(privkey)
 
+# get nonces on all chains
+nonces = {}
+for net, w3 in w3s.items():
+    print(net, 'block', w3.eth.getBlock('latest')['number'])
+    nonces[net] = w3.eth.getTransactionCount(acct.address)
+print('Nonces:', nonces)
+
 print(time.ctime())
 
-for net, w3 in w3s.items():
-    # infura doesn't like chainId==0, so be explicit
-    tx['chainId'] = chainids[net]
+for nonce, tx in txs.items():
+    # populate "missing" key
+    tx['nonce'] = nonce
 
-    # TODO: other ways to specify?..
-    if 'gasPrice' not in tx.keys():
-        tx['gasPrice'] = w3.eth.gasPrice
+    for net, w3 in w3s.items():
+        # skip txs already in the chain
+        if tx['nonce'] <= nonces[net]:
+            print('Transaction with nonce', tx['nonce'], 'already exists on', net, '- skipping...')
+            continue
 
-    signed = acct.signTransaction(tx)
+        # infura doesn't like chainId==0, so be explicit
+        tx['chainId'] = chainids[net]
 
-    txhash = None
-    try:
-        txhash = w3.eth.sendRawTransaction(signed.rawTransaction)
-        print(net, 'txhash', Web3.toHex(txhash))
-    except ValueError as e:
-        errorcode = e.args[0]['code']
+        # TODO: other ways to specify?..
+        if 'gasPrice' not in tx.keys():
+            tx['gasPrice'] = w3.eth.gasPrice
 
-        # 'invalid sender' (everywhere?..) and 'transaction already imported' (kovan)
-        if errorcode != -32000 and errorcode != -32010:
-            raise e
-        else:
-            print(net, 'transaction with nonce', tx['nonce'], 'already exists!..')
+            signed = acct.signTransaction(tx)
+
+            txhash = w3.eth.sendRawTransaction(signed.rawTransaction)
+            print(net, 'tx with nonce', tx['nonce'], 'txhash', Web3.toHex(txhash))
